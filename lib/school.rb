33a334e344c1c94b40e5fea65dcde35d8f4a87a8
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-08 16:39:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  
+attr_accessor :name, :roster
+ 
+ def initialize(name, roster)
+   @name = name
+   @roster = {}
+ end
+ 
+ def add_student(name, grade)
+   @names = []
+   while i = '@grade 1...12'
+   @roster << names.each_with_index {|name, i|
+   puts "#(i+1). #(name)"}
+   i += 1
+ end
+ end
+ 
+ end
+
+  
+  
+  
+end
+
+school = School.new("Bayside High School")
+school.roster
+
+school.add_student("Zach Morris", 9)
+school.roster
