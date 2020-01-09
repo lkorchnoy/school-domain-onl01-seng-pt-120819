2b4a544263c913f14b89af038116b4e8865b02a8
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-09 18:45:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -28,3 +28,17 @@ school.roster
 
 school.add_student("Zach Morris", 9)
 school.roster
+
+
+roster[student_grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
